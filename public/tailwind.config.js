96bd6bb142807c632b1bfe9a793b5a4d2b409466
Ismayil <ismayilov1995@gmail.com>
2021-07-13 22:33:22 +0400

    header ready

--- public/tailwind.config.js
+++ public/tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     extend: {},
     container: {
       center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
     }
   },
   variants: {

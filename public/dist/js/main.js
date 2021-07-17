717e3ec7701cb97c7a4499acb1536697c295aa5b
Ismayil Ismayilov <ismayilov1995@gmail.com>
2021-07-17 11:42:36 +0400

    breaking news interval aded

--- public/dist/js/main.js
+++ public/dist/js/main.js
@@ -1,7 +1,22 @@
 const popup = $("#menu-popup");
 
 $("#menu-btn").click(function () {
-//   console.log("salami");
-//   popup.toggleClass("hidden", [1000]);
-    popup.toggle(200)
+  popup.toggle(200);
 });
+
+var breakingNews = [
+  "Yabancı Turist Sayısı Hedefini Yakaladı Turizm Bakanlığı'nda Yüzler Gülüyor...",
+  "Rəsmən: 2022-ci ilin bolidi təqdim olunub",
+  "L.Həmilton: Çox qorxduq",
+  "M.Ferstappen: Mercedes əlçatmazdır",
+];
+var counter = 0;
+var changingElement = document.getElementById("breaking-news").firstElementChild;
+setInterval(changeIt, 3000);
+function changeIt() {
+  changingElement.innerHTML = breakingNews[counter];
+  counter++;
+  if (counter >= breakingNews.length) {
+    counter = 0;
+  }
+}

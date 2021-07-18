5bd3af7eb1a21cfbb4bb6bac0ea2af29441407fe
Ismayil <ismayilov1995@gmail.com>
2021-07-18 13:22:56 +0400

    custom font

--- public/tailwind.config.js
+++ public/tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"],
+      Lobster: ['Lobster', 'cursive'],
+    },
     container: {
       center: true,
       padding: "1rem",
